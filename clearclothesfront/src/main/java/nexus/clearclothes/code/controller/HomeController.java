843f33c4c9ca0acab4c680d04e8bfb12943a04dc
github2sean <seanzq0331@163.com>
2018-02-09 17:52:45 +0800

    架构

---
+++ clearclothesfront/src/main/java/nexus/clearclothes/code/controller/HomeController.java
@@ -0,0 +1,35 @@
+package nexus.clearclothes.code.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*****************************************
+ * *
+ * @dookay.com Internet make it happen *
+ * ----------- ----------------------- *
+ * dddd ddddd Internet make it happen *
+ * o o o Internet make it happen *
+ * k k k Internet make it happen *
+ * a a a Internet make it happen *
+ * yyyy yyyyy Internet make it happen *
+ * ----------- ----------------------- *
+ * @dookay.com Internet make it happen *
+ * *
+ ****************************************/
+
+
+@RestController
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/HelloWorld")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
